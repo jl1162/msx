322bdb28b077a9b21c0c93dbe6f393c8df3b9b1a
jl1162 <jellarie161@gmail.com>
2025-06-25 05:33:39 +0800

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Login route
-router.post('/login', authController.login);
-
-module.exports = router;
